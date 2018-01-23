57aaa247b18e469b4d4b65f6f1667534624699f5
lucasforworking <35700088+lucasforworking@users.noreply.github.com>
2018-01-23 11:36:16 +0800

    Add OTA
    Arduino IDE OTA and web site update

--- LED_Dimm.ino
+++ LED_Dimm.ino
@@ -3,20 +3,22 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include "index.h"
 
 
 //const byte ADC_PIN = A0;
 //const byte GPIO_4  = 4;
 //const byte GPIO_5  = 5;
-// Only ESP01 Pin number
+//Only ESP01 Pin number
 //const byte LED_PIN = 0;
 const byte PWM_PIN = 2;
 
 // You need to change below const char
-const char* host = "test";			 
+const char* host = "test";       
 const char ssid[] = "ssid";
-const char pass[] = "password";
+const char pass[] = "pass";
 
 ESP8266WebServer server(80);   // 宣告網站伺服器物件與埠號
 ESP8266HTTPUpdateServer httpUpdater;
@@ -49,12 +51,37 @@ void setup() {
   Serial.println("");
   Serial.print("WiFi connected, IP: ");
   Serial.println(WiFi.localIP());  // 顯示ESP8266裝置的IP位址
+   // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+// 主機名稱預設為"esp8266-晶片ID"
+ArduinoOTA.setHostname("test");  // 改成"test"
  
+// 預設無需驗證密碼
+ArduinoOTA.setPassword((const char *)"12345678");  // 密碼設定為"12345678"
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
   server.on ( "/", rootRouter);
   server.on ( "/index.html", rootRouter);
   
-  server.on ("/DIMM", []() 
-  {
+  server.on ("/DIMM", []() {
     String state = server.arg("dimm");
     analogWrite(PWM_PIN, state.toInt());
     Serial.print("PWM_PIN: ");
@@ -78,12 +105,12 @@ void setup() {
   server.begin();
   Serial.println("HTTP server started.");
 
-  MDNS.setInstanceName("Cubie's ESP8266");
+  MDNS.setInstanceName("Test's ESP8266");
   MDNS.addService("http", "tcp", 80);
   Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
 }
 
 void loop() {
+    ArduinoOTA.handle();
   server.handleClient();
 }
-
--- index.h
+++ index.h
@@ -1,7 +1,6 @@
 const char PAGE_INDEX[] PROGMEM = R"=====(
 <!doctype html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,38 +10,37 @@ const char PAGE_INDEX[] PROGMEM = R"=====(
         body {
             font-family: "微軟正黑體", "黑體-繁", sans-serif;
         }
-        #slider {
-            width: 300px;
+         #LED_Dimm {
+            width:  500px;
             margin: 15px;
         }
     </style>
 </head>
-
 <body>
     <h1>請選擇亮度</h1>
     <br>
     雲亮度：
     <div id="LED_Dimm">
-        <input type="radio" id="LED_100" class="SW" value="100" name="SW">
+        <input type="radio" id="LED_100" class="SW" value="100" name="dimm">
         <label for="LED_100"> 100% </label>
-        <input type="radio" id="LED_75" class="SW" value="75" name="SW">
+        <input type="radio" id="LED_75" class="SW" value="75" name="dimm">
         <label for="LED_75"> 75% </label>
-    <input type="radio" id="LED_50" class="SW"  value="50" name="SW">
+    <input type="radio" id="LED_50" class="SW"  value="50" name="dimm">
         <label for="LED_50"> 50% </label>
-    <input type="radio" id="LED_25" class="SW" value="25" name="SW">
+    <input type="radio" id="LED_25" class="SW" value="25" name="dimm">
         <label for="LED_25"> 25% </label>
-    <input type="radio" id="LED_0" class="SW" value="0" name="SW">
+    <input type="radio" id="LED_0" class="SW" value="0" name="dimm">
         <label for="LED_0"> 0% </label>
     </div>
   倒數時間：
     <div id="LED_time">
-      <input type="radio" id="LED_T1H" class="TM" value="60" name="SW">
+      <input type="radio" id="LED_T1H" class="TMR" value="60" name="count">
         <label for="LED_T1H"> 1hr </label>
-        <input type="radio" id="LED_T30M" class="TM" value="30" name="SW">
+        <input type="radio" id="LED_T30M" class="TMR" value="30" name="count">
         <label for="LED_T30M"> 30min </label>
-        <input type="radio" id="LED_T10M" class="TM" value="10" name="SW">
+        <input type="radio" id="LED_T10M" class="TMR" value="10" name="count">
         <label for="LED_T10M"> 10min </label>
-        <input type="radio" id="LED_T5M" class="TM  " value="5" name="SW">
+        <input type="radio" id="LED_T5M" class="TMR" value="5" name="count">
         <label for="LED_T5M"> 5min </label> 
     </div>
     <script src="https://code.jquery.com/jquery-1.12.1.min.js"></script>
@@ -60,16 +58,14 @@ const char PAGE_INDEX[] PROGMEM = R"=====(
     
         $(function () {
             $("#LED_time").buttonset();
-            $(".TM").change(function (evt) {
+            $(".TMR").change(function (evt) {
                 var state = $(this).val();
                 $.post("/TIME", { timevalue: state });
             });
         }); 
     
-    
     </script>
   
 </body>
-
 </html>
 )=====";
